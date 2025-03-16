230e917b84f3953f1c7294d07f0e7c3e954d529a
medbbh <mbabah3450@gmail.com>
2025-03-16 13:27:40 +0000

    add Gunicorn and Swagger API documentation

--- backend/thimar_project/urls.py
+++ backend/thimar_project/urls.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Thimar API",
+        default_version='v1',
+        description="API documentation for Thimar: a productivity tool for Muslims",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,8 +26,13 @@ urlpatterns = [
     path('api/sub-goals/', include('goals.urls')),
     path('api/prayers/', include('tasks.prayer_urls')),
     path('api/tasks/', include('tasks.urls')),
+
+    # Swagger and Redoc endpoints
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
