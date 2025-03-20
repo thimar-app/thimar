9121e7060f8b630b10ae6740dd5dd1cf685fb07d
sidimedmelamine@gmail.com <sidimedmelamine@gmail.com>
2025-03-20 01:36:37 +0000

    instal

--- backend/goals/urls.py
+++ backend/goals/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from .views import (
-    GoalListCreateView, GoalDetailView,GenerateGoalView,
+    GoalListCreateView, GoalDetailView,
     SubGoalListCreateView, SubGoalDetailView
 )
 
@@ -10,8 +10,7 @@ urlpatterns = [
     # Routes pour Goals
     path('', GoalListCreateView.as_view(), name='goal-list-create'),
     path('<uuid:pk>/', GoalDetailView.as_view(), name='goal-detail'),
-    path('generate-goal/', GenerateGoalView.as_view(), name='goal-generate'),
-
+    
     # Routes pour SubGoals
     path('sub-goals/', SubGoalListCreateView.as_view(), name='subgoal-list-create'),
     path('sub-goals/<uuid:pk>/', SubGoalDetailView.as_view(), name='subgoal-detail'),
